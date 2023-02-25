29c356a908ec81b59286066f87f938626fcc3c02
ochafik <ochafik@google.com>
2023-02-25 17:07:16 +0000

    [manifold] Install deps needed by manifold on CI

--- scripts/travis-ci-before-install-linux.sh
+++ scripts/travis-ci-before-install-linux.sh
@@ -7,6 +7,7 @@ PACKAGES2="libboost-all-dev libboost-dev libeigen3-dev libzip-dev"
 PACKAGES3="libxi-dev libxmu-dev qtbase5-dev qtmultimedia5-dev libqt5opengl5-dev libqt5svg5-dev libqt5scintilla2-dev"
 PACKAGES4="libcgal-dev libglew-dev libgmp3-dev libgmp-dev libmpfr-dev"
 PACKAGES5="libdouble-conversion-dev libfontconfig-dev libharfbuzz-dev libopencsg-dev lib3mf-dev"
+PACKAGES6="libomp-dev libassimp-dev git libtbb-dev pkg-config libpython3-dev python3"
 
 if [[ "$DIST" == "xenial" ]]; then
 
@@ -49,4 +50,4 @@ sudo apt-get purge -qq fglrx || true
 sudo apt-cache rdepends libzip4 || true
 sudo apt-get purge -qq libzip4 $(apt-cache rdepends --installed libzip4 | tail -n+3)
 
-sudo apt-get install -qq $PACKAGES1 $PACKAGES2 $PACKAGES3 $PACKAGES4 $PACKAGES5
+sudo apt-get install -qq $PACKAGES1 $PACKAGES2 $PACKAGES3 $PACKAGES4 $PACKAGES5 $PACKAGES6
