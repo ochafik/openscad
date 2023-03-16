893713f77f70eea01137d1a13f955a8aa0c4625f
ochafik <ochafik@google.com>
2023-03-16 04:21:50 +0000

    [manifold] Tweak linux packages

--- scripts/travis-ci-before-install-linux.sh
+++ scripts/travis-ci-before-install-linux.sh
@@ -6,8 +6,7 @@ PACKAGES1="build-essential bison cmake curl flex git-core imagemagick ghostscrip
 PACKAGES2="libboost-all-dev libboost-dev libeigen3-dev libzip-dev"
 PACKAGES3="libxi-dev libxmu-dev qtbase5-dev qtmultimedia5-dev libqt5opengl5-dev libqt5svg5-dev libqt5scintilla2-dev"
 PACKAGES4="libcgal-dev libglew-dev libgmp3-dev libgmp-dev libmpfr-dev"
-PACKAGES5="libdouble-conversion-dev libfontconfig-dev libharfbuzz-dev libopencsg-dev lib3mf-dev"
-PACKAGES6="libomp-dev libassimp-dev libtbb-dev pkg-config libpython3-dev"
+PACKAGES5="libdouble-conversion-dev libfontconfig-dev libharfbuzz-dev libopencsg-dev lib3mf-dev libtbb-dev"
 
 if [[ "$DIST" == "xenial" ]]; then
 
@@ -50,4 +49,4 @@ sudo apt-get purge -qq fglrx || true
 sudo apt-cache rdepends libzip4 || true
 sudo apt-get purge -qq libzip4 $(apt-cache rdepends --installed libzip4 | tail -n+3)
 
-sudo apt-get install -qq $PACKAGES1 $PACKAGES2 $PACKAGES3 $PACKAGES4 $PACKAGES5 $PACKAGES6
+sudo apt-get install -qq $PACKAGES1 $PACKAGES2 $PACKAGES3 $PACKAGES4 $PACKAGES5
